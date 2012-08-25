f2e78b6ea2f975fa617800ed3f70cc86da9a3204
abhimanyuma <abhimanyu.m.a@gmail.com>
2012-08-25 23:34:16 +0530

    Index file changed

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <html>
 <head>
 <title>Answer Me</title>
+ <link href="css/main.css" rel="stylesheet">
 </head>
 <body>
-<div class="boxy shadow"
+<div class="boxy shadow logo"> 
+<h1>Answer Me !</h1>
+</div>
+</body>
 
