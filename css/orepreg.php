6865cbebb0f607e0d0e5f9cbf9b025871e5e2ad8
abhimanyuma <abhimanyu.m.a@gmail.com>
2012-08-26 04:06:29 +0530

    Orep Linking PHP done dirty

---
+++ css/orepreg.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'connect_db.php';
+if (!(isset($_SESSION['userid'])))
+{
+	header( 'Location: login.php' ); 
+}
+$ch= curl_init();
+$url="http://orep.manyu.in/getsessionkey.php?siteid=".siteid."&sitekey=".sitekey;
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+
+$answer=curl_exec($ch);
+curl_close($ch);
+$obj=json_decode($answer);
+if ($obj['res']==false)
+{
+header( 'Location: index.php' ); 
+}
+else
+{
+$ssid=$obj['ssid'];
+header( 'Location: http://orep.manyu.in/login.php?ssid='.$ssid ); 
+}
+?>
+
+
